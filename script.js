0cc99a1b1a77f152ec789bd5d18aa7a3fc58c9ac
Renzo Mogrovejo <rzmogrovejo@Renzos-MacBook-Pro.local>
2021-04-12 00:57:21 -0500

    add script and update index

---
+++ script.js
@@ -0,0 +1,26 @@
+const channels = [
+	{
+		"name": "Latina",
+		"website": "https://www.latina.pe/tvenvivo",
+		"code": {
+			"national": "2",
+			"movistar-hd": "702"
+		}
+	},
+	{
+		"name": "America TV",
+		"website": "https://tvgo.americatv.com.pe/",
+		"code": {
+			"national": "4",
+			"movistar-hd": "704"
+		}
+	},
+	{
+		"name": "TV Perú",
+		"website": "https://www.tvperu.gob.pe/play",
+		"code": {
+			"national": "7",
+			"movistar-hd": "N"
+		}
+	}
+];
