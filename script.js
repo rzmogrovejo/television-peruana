84ef100990893d59df9430d2a59aae002220b626
Renzo Mogrovejo <rzmogrovejo@MacBook-Air-de-Renzo.local>
2021-04-12 02:09:18 -0500

    update channels

--- script.js
+++ script.js
@@ -16,11 +16,35 @@ const channels = [
 		}
 	},
 	{
+		"name": "Panamericana",
+		"website": "https://panamericana.pe/tvenvivo",
+		"code": {
+			"national": "5",
+			"movistar-hd": "705"
+		}
+	},
+	{
 		"name": "TV Perú",
 		"website": "https://www.tvperu.gob.pe/play",
 		"code": {
 			"national": "7",
-			"movistar-hd": "N"
+			"movistar-hd": "707"
 		}
-	}
+	},
+	{
+		"name": "ATV",
+		"website": "https://www.atv.pe/envivo-atv",
+		"code": {
+			"national": "9",
+			"movistar-hd": "709"
+		}
+	},
+	{
+		"name": "Willax",
+		"website": "https://willax.tv/en-vivo/",
+		"code": {
+			"national": "16",
+			"movistar-hd": "716"
+		}
+	},	
 ];
